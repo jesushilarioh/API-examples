b5303386af05c127727aa8ac8367a0bbe7fec7d4
jesushilarioh <jesushilariohernandez@gmail.com>
2017-11-29 10:32:01 -0600

    Nov. 29 2017

--- Javascript/New York Times/Archive API/index.js
+++ Javascript/New York Times/Archive API/index.js
@@ -1,16 +1,51 @@
 $(document).ready(function () {
-    // AJAX Call
-    $.ajax({
-        url: url,
-        method: 'GET'
 
-    }).done(function (result) {
-        // When good things happen.
-        console.log(result);
 
-    }).fail(function (err) {
-        // When bad things happen.
-        console.log(err);
+    $("#month").keyup(function (e) {
+        if (e.keyCode == 13) {
+            var year = $("#year").val();
+            var month = $("#month").val();
 
-    })
+            // AJAX Call
+            var url = "https://api.nytimes.com/svc/archive/v1/" + year + "/" + month + ".json";
+            url += '?' + $.param({
+                'api-key': "<---Your-API-Key--->"
+            });
+            $.ajax({
+                url: url,
+                method: 'GET'
+
+            }).done(function (result) {
+                // When good things happen.
+                for (var i = 0; i < 10; i++) {
+                    console.log(result.response.docs[i]);
+                }
+
+            }).fail(function (err) {
+                // When bad things happen.
+                console.log(err);
+            })
+            console.log(url);
+
+        }
+
+    });
+
+    // $("#month").keyup(function () {
+    //     var year = $("#year").val();
+    //     var month = $("#month").val();
+    //     // AJAX Call
+    //     var url = "https://api.nytimes.com/svc/archive/v1/" + year + "/" + month + ".json";
+    //     url += '?' + $.param({
+    //         'api-key': "937889dd098d40988431171b07ff38e1"
+    //     });
+    //     console.log(url);
+    //
+    // });
+
+    // var month = $("#month").keyup(function () {
+    //     console.log("Month is " + $("#month").val());
+    // });
+
+
 });
